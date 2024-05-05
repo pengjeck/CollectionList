91277703f343455390a66fe9bd857b86b0057ba6
pengjian05 <pengjian05@baidu.com>
2024-05-05 21:52:09 +0800

    add tailwind support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,25 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: '精品大模型课程',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'HuggingFace',
-					autogenerate: { directory: 'HuggingFace' },
-				},
-				{
-					label: 'OpenAI',
-					autogenerate: { directory: 'OpenAI' },
-				},
-				{
-					label: 'Google',
-					autogenerate: { directory: 'Google' }
-				}
-			],
-		}),
-	],
+  integrations: [starlight({
+    title: 'High-Quality Free LLM Courses',
+    customCss: [
+      './src/tailwind.css'
+    ],
+    social: {
+      github: 'https://github.com/withastro/starlight'
+    },
+    sidebar: [{
+      label: 'Courses',
+      autogenerate: {
+        directory: 'Courses'
+      }
+    }]
+  }), tailwind({
+    applyBaseStyles: false
+  })]
 });
